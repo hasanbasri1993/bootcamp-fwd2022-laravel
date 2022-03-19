894b2d6cf775e95df69d7cb453900bc6a5a036fb
Hasan Basri <hasanbasri1493@gmail.com>
2022-03-19 13:55:54 +0700

    add migration tables

---
+++ database/migrations/2022_03_19_064837_create_transaction_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->integer('transaction_id');
+            $table->integer('config_payment');
+            $table->string('fee_doctor');
+            $table->string('fee_specialist');
+            $table->string('fee_hospital');
+            $table->string('sub_total');
+            $table->string('vat');
+            $table->string('total');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
